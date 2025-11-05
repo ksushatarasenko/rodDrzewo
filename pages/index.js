6c6cee319caf0cee7e812ac4f05fb665abb43889
Oksana <ksushatarasenko@gmail.com>
2025-11-05 21:46:12 +0100

    new

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   const row1 = [11,1,2,9,12]; // дети
   const row2 = [10,3,4,13,14]; // родители
   const row3 = [5,6,7,8]; // бабушки и дедушки
-  const row4 = []; // например, прабабушки и прадедушки
+  const row4 = [18,19,20,21]; // например, прабабушки и прадедушки
 
   const getPeople = (ids) => family.filter((p) => ids.includes(p.id));
 
